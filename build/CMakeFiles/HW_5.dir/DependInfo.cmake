b48bdc251af88eb7bcb19863aa20064899b3b52d
robertbiv <robert@robertb.me>
2025-11-14 21:05:21 +0000

    Initial plan for creating README

---
+++ build/CMakeFiles/HW_5.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/runner/work/infix-to-postfix-evaluator/infix-to-postfix-evaluator/calcimpl.cpp" "CMakeFiles/HW_5.dir/calcimpl.cpp.o" "gcc" "CMakeFiles/HW_5.dir/calcimpl.cpp.o.d"
+  "/home/runner/work/infix-to-postfix-evaluator/infix-to-postfix-evaluator/calctest.cpp" "CMakeFiles/HW_5.dir/calctest.cpp.o" "gcc" "CMakeFiles/HW_5.dir/calctest.cpp.o.d"
+  "" "HW_5" "gcc" "CMakeFiles/HW_5.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/HW_5.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW_5.dir/link.d"
+  "CMakeFiles/HW_5.dir/calcimpl.cpp.o"
+  "CMakeFiles/HW_5.dir/calcimpl.cpp.o.d"
+  "CMakeFiles/HW_5.dir/calctest.cpp.o"
+  "CMakeFiles/HW_5.dir/calctest.cpp.o.d"
+  "HW_5"
+  "HW_5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
